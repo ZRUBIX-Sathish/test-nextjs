fa73c180490004e2e23921ce8f7363d8d8a0392d
sathish <sathish@zrubix.com>
2026-03-03 16:03:31 +0530

    backend hosted

---
+++ functions/test_function/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { IncomingMessage, ServerResponse } = require("http");
+
+/**
+ * 
+ * @param {IncomingMessage} req 
+ * @param {ServerResponse} res 
+ */
+module.exports = (req, res) => {
+	var url = req.url;
+
+	switch (url) {
+		case '/':
+			async function test() {
+				await fetch("https://play.svix.com/in/e_c63U3zsLItnyKVITaMA4seTzLwq/", {
+					"method": "POST",
+					"headers": {
+						"Content-Type": "application/json"
+					},
+					"body": JSON.stringify({
+						"status": "200 success",
+						"triggering": "test-nextjs"
+					})
+				})
+			}
+			test()
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write('<h1>Hello from index.js<h1>');
+			break;
+		default:
+			res.writeHead(404);
+			res.write('You might find the page you are looking for at "/" path');
+			break;
+	}
+	res.end();
+};
