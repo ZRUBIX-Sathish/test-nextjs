e47d9942d244a20d906124730f5f288b18254448
sathish <sathish@zrubix.com>
2026-03-03 17:13:20 +0530

    V6

--- functions/test_function/index.js
+++ functions/test_function/index.js
@@ -12,7 +12,7 @@ module.exports = (req, res) => {
 
 	switch (url) {
 		case '/':
-			// V2
+			// V6
 			async function test() {
 				await fetch("https://play.svix.com/in/e_c63U3zsLItnyKVITaMA4seTzLwq/", {
 					"method": "POST",
